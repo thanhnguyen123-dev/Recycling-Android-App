1bbd601c4716bc18929311ec03a27214ee52ae01
Julius Liem <u7724204@anu.edu.au>
2024-04-10 11:34:54 +1000

    Added a model for recycled item

---
+++ RecycleMe/app/src/main/java/com/example/recycleme/RecycledItem.java
@@ -0,0 +1,52 @@
+package com.example.recycleme;
+
+import java.util.Objects;
+
+public class RecycledItem {
+    private String item;
+
+    private int id;
+    private String brand;
+    private String material;
+    private Double value;
+
+    public RecycledItem(String item, String brandName, String material, Double value) {
+        this.item = item;
+        this.brand = brandName;
+        this.material = material;
+        this.value = value;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public String getBrandName() {
+        return brand;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecycledItem that = (RecycledItem) o;
+        return id == that.id && Objects.equals(item, that.item) && Objects.equals(brand, that.brand) && Objects.equals(material, that.material) && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item, id, brand, material, value);
+    }
+}
