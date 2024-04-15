31e8cfbda5d747c3aa25e418a3e4d3f382c922f1
Julius Liem <u7724204@anu.edu.au>
2024-04-15 12:53:21 +1000

    Added junit dependencies to the libs folder

--- RecycleMe/app/build.gradle.kts
+++ RecycleMe/app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.gson)
+    implementation(files("libs/JUnit4/hamcrest-core-1.3.jar"))
+    implementation(files("libs/JUnit4/junit-4.13.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
