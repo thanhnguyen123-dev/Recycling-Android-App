720767a5f2d7bc33628074dd9dcaaa58691f56f0
Devansu Mahendra Yadav <u7761531@anu.edu.au>
2024-05-06 00:41:31 +1000

    fix: tokenize multi-word item names correctly

--- RecycleMe/app/src/main/java/com/example/recycleme/search/Tokenizer.java
+++ RecycleMe/app/src/main/java/com/example/recycleme/search/Tokenizer.java
@@ -41,11 +41,25 @@ public class Tokenizer {
         return remainingChars >= 3 && searchQuery.startsWith("and");
     }
 
+    private boolean nextCharsFormAnd(int startIndex) {
+        if (startIndex + 3 > searchQuery.length()) {
+            return false;
+        }
+        return searchQuery.substring(startIndex, startIndex + 3).equalsIgnoreCase("and");
+    }
+
     private Token scanString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int idx = 0; idx < searchQuery.length() && Character.isLetter(searchQuery.charAt(idx)); idx++) {
-            stringBuilder.append(searchQuery.charAt(idx));
+        for (int idx = 0; idx < searchQuery.length(); idx++) {
+            char currentChar = searchQuery.charAt(idx);
+            if(Character.isLetter(searchQuery.charAt(idx))) {
+                stringBuilder.append(currentChar);
+            } else if (currentChar == ' ' && !nextCharsFormAnd(idx + 1)) {
+                stringBuilder.append(currentChar);
+            } else {
+                break;
+            }
         }
         return new Token(Token.TokenType.ITEM, stringBuilder.toString());
     }
