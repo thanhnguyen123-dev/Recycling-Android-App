95e71b479a8432305dfbcd1ac8bb98204c8eb0b0
ExrosZ <167173157+ExrosZ@users.noreply.github.com>
2024-05-11 15:26:23 +1000

    More minor code fixes

--- RecycleMe/app/src/main/java/com/example/recycleme/dao/FirebaseRecycledItemDAO.java
+++ RecycleMe/app/src/main/java/com/example/recycleme/dao/FirebaseRecycledItemDAO.java
@@ -32,7 +32,7 @@ public class FirebaseRecycledItemDAO implements RecycledItemDAO {
 
     private final String FILE_NAME;
     private Gson gson;
-    private Future <List<RecycledItem>> allRecycledItems;
+    private Future <List<RecycledItem>> allRecycledItemsFuture;
 
     private Future<List<RecycledItem>> future;
 
@@ -47,7 +47,7 @@ public class FirebaseRecycledItemDAO implements RecycledItemDAO {
         this.gson = new Gson();
         this.context = context;
         this.f = f;
-        this.allRecycledItems = this.getAllRecycledItemsHelper();
+        this.allRecycledItemsFuture = this.getAllRecycledItemsHelper();
     }
 
 
