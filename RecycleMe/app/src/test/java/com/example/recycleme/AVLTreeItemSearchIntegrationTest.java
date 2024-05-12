de2fca48714ce0a5380d6044278bef035394aa54
Julius Liem <u7724204@anu.edu.au>
2024-05-12 17:57:41 +1000

    Added integration and parameterized test

---
+++ RecycleMe/app/src/test/java/com/example/recycleme/AVLTreeItemSearchIntegrationTest.java
@@ -0,0 +1,78 @@
+package com.example.recycleme;
+
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.recycleme.model.RecycledItem;
+import com.example.recycleme.search.SearchExp;
+import com.example.recycleme.search.SearchQueryParser;
+import com.example.recycleme.search.Tokenizer;
+import com.example.recycleme.util.tree.AVLTreeItem;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class AVLTreeItemSearchIntegrationTest {
+    private AVLTreeItem avlTreeItem;
+    private SearchQueryParser searchQueryParser;
+    private String query;
+    private int expectedSize;
+    private String expectedItem;
+    private String expectedBrand;
+    private String expectedMaterial;
+
+
+    public AVLTreeItemSearchIntegrationTest(String query, int expectedSize, String expectedItem, String expectedBrand, String expectedMaterial) {
+        this.query = query;
+        this.expectedSize = expectedSize;
+        this.expectedItem = expectedItem;
+        this.expectedBrand = expectedBrand;
+        this.expectedMaterial = expectedMaterial;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "dehumidifier and #Philips and @Plastic", 1, "Dehumidifier", "Philips", "Plastic" },
+                { "Speaker and #Sennheiser and @Aluminum", 1, "Speaker", "Sennheiser", "Aluminum" },
+                { "Green Bean and #Sennheiser and @Organic", 1, "Green Bean", "Sennheiser", "Organic" },
+                { "Dehumidifier and @Plastic", 2, "Dehumidifier", "Philips", "Plastic" },
+                { "#sennheiser", 2, "Green Bean", "Sennheiser", "Organic"},
+                { "error and #error and @error", 0, null, null, null }
+        });
+    }
+
+    @Before
+    public void setUp() {
+        avlTreeItem = new AVLTreeItem();
+
+        avlTreeItem.insert(new RecycledItem(1, "Dehumidifier", "Philips", "Plastic", 100.0));
+        avlTreeItem.insert(new RecycledItem(2, "Speaker", "Sennheiser", "Aluminum", 50.0));
+        avlTreeItem.insert(new RecycledItem(3, "Green Bean", "Sennheiser", "Organic", 50.0));
+        avlTreeItem.insert(new RecycledItem(4, "dehumidifier", "Shilips", "Plastic", 100.0));
+    }
+
+    @Test
+    public void testSearch() throws SearchQueryParser.InvalidQueryException {
+        searchQueryParser = new SearchQueryParser(new Tokenizer(this.query));
+
+        SearchExp searchExp = searchQueryParser.parseSearchQuery();
+        List<RecycledItem> result = searchExp.evaluateSearchExp(this.avlTreeItem);
+
+        assertEquals(expectedSize, result.size());
+
+        if (!result.isEmpty()) {
+            RecycledItem firstItem = result.get(0);
+            assertEquals(expectedItem, firstItem.getItem());
+            assertEquals(expectedBrand, firstItem.getBrandName());
+            assertEquals(expectedMaterial, firstItem.getMaterial());
+        }
+    }
+}
---
+++ RecycleMe/app/src/test/java/com/example/recycleme/Cart_UserTreeIntegrationTest.java
@@ -0,0 +1,77 @@
+package com.example.recycleme;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.example.recycleme.cart.Cart;
+import com.example.recycleme.cart.UserTree;
+import com.example.recycleme.model.RecycledItem;
+import com.example.recycleme.model.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Cart_UserTreeIntegrationTest {
+    private Cart cart;
+    private UserTree userTree;
+
+    @Before
+    public void setUp() {
+        this.cart = Cart.getInstance();
+        cart.clear();
+
+        this.userTree = UserTree.getInstance();
+        userTree.clear();
+
+    }
+
+    @Test
+    public void testAddItemsToCartAndUserTreeEmpty() {
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        List<RecycledItem> currentItem = cart.getItems();
+        userTree.addItems(currentTime, currentItem);
+
+        List<RecycledItem> userTreeItem = userTree.searchItem(currentTime);
+        assertTrue(userTreeItem.isEmpty());
+    }
+
+    @Test
+    public void testAddItemsToCartAndUserTreeDuplicate() {
+        RecycledItem recycledItem = new RecycledItem(1, "Bottle", "Coca", "Plastic", 10.0);
+        RecycledItem recycledItem2 = new RecycledItem(1, "Bottle", "Coca", "Plastic", 10.0);
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        cart.addItem(recycledItem);
+        cart.addItem(recycledItem2);
+
+        List<RecycledItem> cartItem = cart.getItems();
+        userTree.addItems(currentTime, cartItem);
+
+        // verify that duplicate items are added to usertree
+        List<RecycledItem> userTreeItems = userTree.searchItem(currentTime);
+        assertEquals(cartItem.size(), userTreeItems.size());
+    }
+
+    @Test
+    public void testRemoveItemsFromCartAndUserTreeNonExistentItem() {
+        RecycledItem recycledItem = new RecycledItem(1, "Bottle", "Coca", "Plastic", 10.0);
+
+        cart.addItem(recycledItem);
+        LocalDateTime time = LocalDateTime.now();
+
+        userTree.addItems(time, cart.getItems());
+
+        // remove the item from the cart
+        cart.removeItem(recycledItem);
+
+        // verify that the item is still in the userTree
+        List<RecycledItem> treeList = userTree.searchItem(time);
+
+        assertEquals(treeList.get(0), recycledItem);
+    }
+
+}
